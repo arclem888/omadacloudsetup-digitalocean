4ff8af7b4e749ff0dbe311b21ff196f920d4c10b
Arclem <61035762+arclem888@users.noreply.github.com>
2023-10-06 22:57:43 +0800

    Update installomada.sh
    added new version v5.12.7

--- installomada.sh
+++ installomada.sh
@@ -22,7 +22,7 @@ sudo apt-get -y install gdebi-core
 
 echo "downloading omada sdn controller"
 cd
-sudo wget https://static.tp-link.com/upload/software/2023/202301/20230130/Omada_SDN_Controller_v5.8.4_Linux_x64.deb
+sudo wget https://firebasestorage.googleapis.com/v0/b/arclemwifipublic/o/omada_v5.12.7_linux_x64_20230905160956_1695177653350.deb?alt=media&token=ed0d8236-07b7-48cd-9210-34b8c704f43d
 
 echo "now installing sdn controller"
 cd
